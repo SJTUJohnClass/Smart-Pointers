4eb2555de2b31b29e8622f71d6a76c032b4409bc
DarkSharpness_XzY <2040703891@qq.com>
2023-11-21 19:07:44 +0800

    Upd(shared_ptr): Complete the template.

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -1,4 +1,42 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
 
+template <class _Tp> // Template type parameter
+struct SharedPtr {
+  private:
+    // Add whatever you want to add.
+
+  public:
+    /**
+     * @brief Constructors and assignments of SharedPtr.
+     */
+
+    SharedPtr();
+    ~SharedPtr();
+
+    SharedPtr(const SharedPtr &);
+    SharedPtr(SharedPtr &&);
+
+    SharedPtr& operator = (const SharedPtr &);
+    SharedPtr& operator = (SharedPtr &&);
+
+    explicit SharedPtr(_Tp *);
+
+    /**
+     * @brief Please implement all these member functions.
+     */
+
+    _Tp *get() const;
+    std::size_t use_count() const;
+
+    _Tp &operator * () const;
+    _Tp *operator ->() const;
+
+    void reset();
+    void reset(_Tp *);
+
+    int *use_count() const;
+};
+
+
 #endif //SHARED_PTR
